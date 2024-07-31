5758eb19d1307c00a34da70a3fcf8c3c53950367
ManashSwain <manash0802@gmail.com>
2024-07-31 11:27:12 +0530

    implemented async logics

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,7 +1,53 @@
-const asyncHandler = (requestHandler)=>{
-  (req,res,next)=>{
-    Promise.resolve(requestHandler(req,res,next)).catch((err)=>next(err))
-  }
-}
+// Aync  handler using promises 
+ const asynhandler = (requestHandler)=>{
+     return (req,res,next)=>{
+       Promise.resolve(requestHandler(req,res,next)).catch((error)=>next(error))
+    }
+ }
 
-export {asyncHandler}
+ export {asynhandler}
+
+// Async handler using try catch 
+
+// const asyncHandler = (requestHandler)=> async (req,res,next)=>{
+//    try {
+//      await requestHandler(req,res,next)
+//    } catch (error) {
+//      res.status(error.status || 500).json({
+//         success : false ,
+//         message : error.message
+//      })
+//    }
+// }
+
+// Basic Function 
+// const asyncHandler = ()=>{}
+
+// Higher order function 
+// const asyncHandler = () => {()=>{}}
+
+// simplified syntax 
+// const asyncHandler = ()=>()=>{}
+
+/*
+Example :
+// using try catch 
+    const asyncHandler1 = (requestHandler)=> async(req,res,next)=>{
+        try {
+           await requestHandler(req,res,next) 
+        } catch (error) {
+        res.status(error.status || 500).json({
+            success : false ,
+            message : error.message
+        }) 
+        }
+    }
+
+    // using promises 
+
+    const asyncHandler2 = (requestHandler)=>{
+        return (req,res,next)=>{
+         Promise.resolve(requestHandler(req,res,next)).catch((error)=>next(error))
+        }
+    }
+*/
